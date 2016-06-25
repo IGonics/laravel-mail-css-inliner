3731e321becaccbc53db10c25a0ec6c774cfe824
ggordon <ggg_gilroy@yahoo.com>
2016-06-25 03:45:46 -0300

    Added MailCSS Inliner Swift Mailer Plugin

---
+++ src/MailCssInlinerPlugin.php
@@ -0,0 +1,76 @@
+ <?php
+
+namespace IGonics\LaravelMailCssInliner;
+
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+
+class MailCssInlinerPlugin implements \Swift_Events_SendListener
+{
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @param array $options options defined in the configuration file.
+     */
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @param Swift_Events_SendEvent $evt
+     */
+    public function beforeSendPerformed(\Swift_Events_SendEvent $evt)
+    {
+        $message = $evt->getMessage();
+
+        $converter = new CssToInlineStyles();
+        $this->applySettings($converter);
+
+        if ($message->getContentType() === 'text/html' ||
+            ($message->getContentType() === 'multipart/alternative' && $message->getBody()) ||
+            ($message->getContentType() === 'multipart/mixed' && $message->getBody())
+        ) {
+            $message->setBody($converter->convert($message->getBody()));
+        }
+
+        foreach ($message->getChildren() as $part) {
+            if (strpos($part->getContentType(), 'text/html') === 0) {
+                
+                $part->setBody($converter->convert($part->getBody()));
+            }
+        }
+    }
+
+    /**
+     * Applies the configuration settings.
+     *
+     * @param CssToInlineStyles $converter
+     */
+    private function applySettings(CssToInlineStyles $converter)
+    {
+        // Always enabled because there is no way to specify an external style sheet
+        // when using this plugin
+        // $converter->setUseInlineStylesBlock();
+
+        // if ($this->options['strip-styles']) {
+        //     $converter->setStripOriginalStyleTags();
+        // }
+
+        // if ($this->options['strip-classes']) {
+        //     $converter->setCleanup();
+        // }
+    }
+
+    /**
+     * Do nothing
+     *
+     * @param Swift_Events_SendEvent $evt
+     */
+    public function sendPerformed(\Swift_Events_SendEvent $evt)
+    {
+        // Do Nothing
+    }
+}
